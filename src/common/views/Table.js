a0911d23a9d03a34ea8110d10c579f43631659e9
Shoeboxam <shoeboxam@gmail.com>
2018-03-12 16:45:30 -0500

    Add basic table

---
+++ src/common/views/Table.js
@@ -0,0 +1,66 @@
+import m from 'mithril'
+
+import {varColor, selVarColor, mergeAttributes} from "../common";
+
+// Interface specification
+//
+// ```
+// m(Table, {
+//     id: id (String),
+//     headers: ['col1Header', 'col2Header'],
+//     data: [['row1col1', 'row1col2'], ['row2col1', 'row2col2']],
+//     activeRow: 'row1col1',
+//     onclickRow: (row) => console.log(row + " was clicked."),
+//     checkboxes: Set(['row1col1']),
+//     onclickCheckbox: (checkbox) => console.log(row + "was checked/unchecked"),
+//     })
+// ```
+
+export default class Table {
+
+    view(vnode) {
+        let {id, headers, data, attrsAll, attrsRows} = vnode.attrs;
+
+        // Interactive rows and checkboxes
+        let {activeRow, onclickRow} = vnode.attrs;
+        let {checkboxes, onclickCheckbox} = vnode.attrs;
+
+        let allChecked = false;
+        if (checkboxes) allChecked = data.length === checkboxes.size;
+        
+        let setAllChecked = (checked) => {
+            // turn on or off all checks
+            data.map((row) => {
+                if (checked !== checkboxes.has(row[0])) onclickCheckbox(row[0], checked)
+            });
+        };
+
+        let headerDiv = headers ? m('tr', {style: {width: '100%'}}, [
+            ...headers.map((header) => m('th', header)),
+            checkboxes ? m('td', m('input[type="checkbox"]', {
+                onclick: m.withAttr("checked", setAllChecked),
+                checked: allChecked
+            })) : undefined
+        ]) : undefined;
+
+        return m(`table#${id}`, mergeAttributes({style: {width: '100%'}}, attrsAll),
+            // rows
+            [headerDiv, ...data.map((row) => {
+                return m('tr', mergeAttributes({
+                        style: {'background-color': row[0] === activeRow ? selVarColor : varColor},
+                        onclick: () => onclickRow(row[0])
+                    }, attrsRows),
+
+                    // columns
+                    [
+                        ...row.map((item) => m('td', item)),
+                        checkboxes ? m('td', m('input[type="checkbox"]', {
+                            onclick: m.withAttr("checked", (checked) => onclickCheckbox(row[0], checked)),
+                            checked: checkboxes.has(row[0])
+                        })) : undefined
+                    ]
+                )
+            })]
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import m from 'mithril';
+import Table from './common/views/Table';
+import * as common from './common/common';
 
 class Main {
     view() {
-        return m('h1', 'test');
+        return m(Table, {
+            headers: ['H1', 'H2', 'H3'],
+            data: [
+                ['var1', 'descriptive stuff', 'string'],
+                ['var1', 'descriptive stuff', 'string']
+            ]
+        })
     }
 }
 
