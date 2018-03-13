ac3aa0645802bfaf780264ed2a0b816f154600a1
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2018-03-13 12:57:56 -0500

    break out components into separate routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import m from 'mithril';
-import Table from './common/views/Table';
-import * as common from './common/common';
 
-class Main {
+import * as common from './common/common';
+import Table from './common/views/Table';
+
+class Editor {
     view() {
         return m(Table, {
             headers: ['H1', 'H2', 'H3'],
@@ -12,8 +13,24 @@ class Main {
                 ['var1', 'descriptive stuff', 'string'],
                 ['var1', 'descriptive stuff', 'string']
             ]
-        })
+        });
     }
 }
 
-m.mount(document.body, Main);
+class Report {
+    view() {
+        return m('div', 'report');
+    }
+}
+
+class Data {
+    view() {
+        return m('div', 'data');
+    }
+}
+
+m.route(document.body, '/', {
+    '/': Editor,
+    '/report': Report,
+    '/data': Data
+});
