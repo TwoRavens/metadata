cc92e1cc3c58910e2ed1c5c33f6f6595d548933f
Marcus D <MarcusD.9234@gmail.com>
2018-03-13 13:19:37 -0500

    working version; displays graphs for selected

--- scripts/imagesFromJSON.py
+++ scripts/imagesFromJSON.py
@@ -10,8 +10,38 @@ pprint(data)	#data loaded
 print("\n\n\nended\n")
 
 pprint(data["variables"]["ended"])	#sample data access
+print(data["variables"]["ended"])
+
+print(data["variables"]["ended"]["plotvalues"])
 
 
-plt.plot([1,2,3,4])
-plt.ylabel('some numbers')
-plt.show()
+print(len(data["variables"]))
+
+#~ plt.plot(data["variables"]["lgdpenl1"]["plotx"],data["variables"]["lgdpenl1"]["ploty"])
+#~ plt.ylabel('some numbers')
+#~ plt.show()
+
+#~ print(data[1])
+
+for keys in data["variables"]:
+	if "plottype" in data["variables"][keys]:
+		if data["variables"][keys]["plottype"] == "continuous":
+			plt.plot(data["variables"][keys]["plotx"], data["variables"][keys]["ploty"])
+			plt.title(keys)
+			#~ print(keys)
+			#~ print("cont")
+			if (keys < "cd"):	#temp limit plot
+				plt.show()
+			plt.close()
+		elif data["variables"][keys]["plottype"] == "bar":
+			barKeys = data["variables"][keys]["plotvalues"].keys()
+			barVals = data["variables"][keys]["plotvalues"].values()
+			#~ print(keys)
+			#~ print(barKeys)
+			#~ print(barVals)
+			#~ print(range(1, len(barKeys) + 1))
+			#~ print("bar")
+			plt.bar(range(1, len(barKeys) + 1), barVals, tick_label=barKeys, width=0.8, color = ["blue"])
+			if (keys == "ended"):
+				plt.show()
+			plt.close()
